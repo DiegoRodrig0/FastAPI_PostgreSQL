9b53a0b995bc83d3e4de5994453b3fb90d0f90d8
Diego Rodrigo <rodrigoteixeira06@hotmail.com>
2026-08-06 14:42:10 -0300

    Estrutura inicial da API

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from app.routers import (
+    consulta,
+    equipe,
+    especialidade,
+    paciente,
+    profissionais,
+    unidade_funcional,
+)
+
+app = FastAPI()
+
+app.include_router(especialidade.router)
+
+app.include_router(unidade_funcional.router)
+
+app.include_router(profissionais.router)
+
+app.include_router(equipe.router)
+
+app.include_router(consulta.router)
+
+app.include_router(paciente.router)
