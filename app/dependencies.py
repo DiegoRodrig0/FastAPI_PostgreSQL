df0006da42c5aff3c55e77a866c250f058d5d60d
Diego Rodrigo <rodrigoteixeira06@hotmail.com>
2026-08-07 16:00:44 -0300

    Adicionando verificação para o endpoint do usuário logado

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,15 @@
-from app.database import SessionLocal
+import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
+from app import models
+from app.database import SessionLocal
+from app.security import verificar_token
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/login"
+)
 
 def get_db():
     db = SessionLocal()
@@ -9,3 +19,38 @@ def get_db():
 
     finally:
         db.close()
+
+def get_usuario_atual(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_db)  # noqa: B008
+):
+    try:
+        payload = verificar_token(token)
+
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=401,
+            detail="Token inválido"
+        )
+
+    usuario_id = payload.get("sub")
+
+    if usuario_id is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Token inválido"
+        )
+
+    usuario = (
+        db.query(models.Usuario)
+        .filter(models.Usuario.id == int(usuario_id))
+        .first()
+    )
+
+    if usuario is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Usuário não encontrado"
+        )
+
+    return usuario
