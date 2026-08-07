c7861b882911471915259fda994b24df9dc766b3
Diego Rodrigo <rodrigoteixeira06@hotmail.com>
2026-08-07 15:52:50 -0300

    Adicionando funções para criar e verificar token JWT

--- app/security.py
+++ app/security.py
@@ -1,4 +1,13 @@
+import os
+
+import jwt
+from dotenv import load_dotenv
 from pwdlib import PasswordHash
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 
 password_hash = PasswordHash.recommended()
 
@@ -7,3 +16,17 @@ def gerar_hash(senha: str):
 
 def verificar_senha(senha: str, senha_hash: str):
     return password_hash.verify(senha, senha_hash)
+
+def criar_token(data: dict):
+    return jwt.encode(
+        data,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+def verificar_token(token: str):
+    return jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM]
+    )
