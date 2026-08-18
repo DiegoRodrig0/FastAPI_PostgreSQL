e186e3a6c1da2a1529ced16ddc9d85c07703a715
Diego Rodrigo <rodrigoteixeira06@hotmail.com>
2026-08-18 15:00:33 -0300

    Adicionando teste com autenticação

--- tests/test_profissionais.py
+++ tests/test_profissionais.py
@@ -8,3 +8,26 @@ def test_profissionais_sem_token():
     response = client.get("/profissional")
 
     assert response.status_code == 401
+
+def test_profissionais_com_token():
+
+    login = client.post(
+        "/auth/login",
+        data={
+            "username": "test@email.com.br",
+            "password": "test12345"
+        }
+    )
+
+    assert login.status_code == 200
+
+    token = login.json()["access_token"]
+
+    response = client.get(
+        "/profissional",
+        headers={
+            "Authorization": f"Bearer {token}"
+        }
+    )
+
+    assert response.status_code == 200
